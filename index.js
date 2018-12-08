c24777e11bf0bae2ec3a0472800551b85f7a4aab
Danijel Maksimovic <da.maksimovic@levi9.com>
2018-12-08 11:11:48 +0100

    Fix formatting in index.js file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const LengthDirective = require("./directives/LengthDirective");
-const NotEmptyDirective = require("./directives/NotEmptyDirective");
-const RangeDirective = require("./directives/RangeDirective");
-const EmailDirective = require("./directives/EmailDirective");
-const RegexDirective = require("./directives/RegexDirective");
-const DateDirective = require("./directives/DateDirective");
-const UrlDirective = require("./directives/UrlDirective");
-const JwtDirective = require("./directives/JwtDirective");
+const LengthDirective = require('./directives/LengthDirective');
+const NotEmptyDirective = require('./directives/NotEmptyDirective');
+const RangeDirective = require('./directives/RangeDirective');
+const EmailDirective = require('./directives/EmailDirective');
+const RegexDirective = require('./directives/RegexDirective');
+const DateDirective = require('./directives/DateDirective');
+const UrlDirective = require('./directives/UrlDirective');
+const JwtDirective = require('./directives/JwtDirective');
 
 module.exports = {
   LengthDirective,
